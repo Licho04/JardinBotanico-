a58ed20b88bb5671e20d4a5c78ec98f290f73e51
Licho04 <luisenrique.lemm001@gmail.com>
2025-11-08 20:14:06 -0600

    Solo API + Recursos

--- plantas/eliminar_planta.php
+++
@@ -1,7 +0,0 @@
-<?php
-include('../configuracion/conexion.php');
-$nombre = $_POST['nombre'];
-$sql = "DELETE FROM plantas WHERE nombre = '$nombre'";
-$resultado = mysqli_query($conexion, $sql);
-echo $resultado ? "ok" : "error";
-?>
